430f0ce5164795049aa44110f0eab2d7b0b0e329
Tony Sebastian <48911909+tonyzebastian@users.noreply.github.com>
2023-09-16 17:58:14 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-/** @type {import('next').NextConfig} */
-  const nextConfig = {
-    reactStrictMode: true,
-    
-  }
-  module.exports = nextConfig
+module.exports = {
+  experimental: {
+    images: true,
+  },
+ reactStrictMode: true,
+};
