6e9612e5e99735208fdffbdab333d4e2c3720816
Tony Sebastian <48911909+tonyzebastian@users.noreply.github.com>
2023-09-16 15:21:21 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
 # move to site folder
-echo CD-ing to site folder..
-cd ./site
+#echo CD-ing to site folder..
+#cd .git
 
 # build and export as static html site
 echo Building and Exporting..
-npm run export
+npm run build
+#npm run export
 
 # copy out folder to docs folder
-echo Moving files to root folder..
-rm -rf ../_next
-cp -R ./out/* ../
+#echo Moving files to root folder..
+#rm -rf ../_next
+#cp -R ./out/* ../
 
 echo Moving to root directory
 cd ..
